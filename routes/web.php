bcfd1671674b8d9bd9b5f792c87356568b688181
SUTISNA10 <sutisna10chuza@gmail.com>
2023-06-21 14:13:25 +0700

    Bagian USER dan ADMIN, Menginstall dan mengatur Auth

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,10 @@ Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('checkout-success');
 
 Route::prefix('admin')
+    ->middleware(['auth','admin'])
     ->namespace('admin')
     ->group(function(){
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
+
+Auth::routes();
