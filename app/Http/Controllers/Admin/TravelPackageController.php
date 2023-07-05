d6cefc65408a25e5895903dcf263364c81ca644e
SUTISNA10 <sutisna10chuza@gmail.com>
2023-07-06 06:45:18 +0700

    Bagian CRUD Travel, Membuat Bagian delete atau destroy

--- app/Http/Controllers/Admin/TravelPackageController.php
+++ app/Http/Controllers/Admin/TravelPackageController.php
@@ -80,6 +80,9 @@ class TravelPackageController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $item = TravelPackage::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('travel-package.index');
     }
 }
